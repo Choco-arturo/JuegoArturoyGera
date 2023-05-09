ec68c21e9e6343619f5b4c6572569ab59b50dae8
Choco-arturo <arturo.gonzalez@uniat.edu.mx>
2023-05-08 23:14:18 -0700

    Enemigo

---
+++ Assets/Arturo/Player/Scripts/CombateCaC.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CombateCaC : MonoBehaviour
+{
+    [SerializeField] private Transform controladorGolpe;
+    [SerializeField] private float radiGolpe;
+    [SerializeField] private float danoGolpe;
+    [SerializeField] private float tiempoEntreAtaque;
+    [SerializeField] private float tiempoSiquienteAtaque;
+
+    private Animator animator;
+
+    private void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        if(tiempoSiquienteAtaque> 0)
+        {
+            tiempoSiquienteAtaque -= Time.deltaTime;
+        }
+
+        if(Input.GetButtonDown("Fire1"))
+        {
+            Golpe();
+            tiempoSiquienteAtaque = tiempoEntreAtaque;
+        }
+    }
+
+
+    private void Golpe()
+    {
+        animator.SetTrigger("Golpe");
+
+        Collider2D[] objetos = Physics2D.OverlapCircleAll(controladorGolpe.position, radiGolpe);
+
+        foreach(Collider2D colisionador in objetos)
+        {
+            if(colisionador.CompareTag("Enemigo"))
+            {
+                colisionador.transform.GetComponent<enemigos>().TomarDano(danoGolpe);
+            }
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(controladorGolpe.position, radiGolpe);
+    }
+}
