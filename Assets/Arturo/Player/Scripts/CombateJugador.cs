40f216ab5509ed84ecb74b93b1de8e785bc17717
Gefeyafa <gerardo.cervantes@uniat.edu.mx>
2023-05-30 00:08:55 -0700

    trampas

--- Assets/Arturo/Player/Scripts/CombateJugador.cs
+++ Assets/Arturo/Player/Scripts/CombateJugador.cs
@@ -14,5 +14,14 @@ public class CombateJugador : MonoBehaviour
             Destroy(gameObject);
         }
     }
-    
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Trap"))
+        {
+            vida = vida - 150f;
+            Destroy(gameObject);
+        }
+    }
+
 }
