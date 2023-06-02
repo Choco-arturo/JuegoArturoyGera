3d3c10cb4e3af9066c2ed1cd7e0a9893007aace9
Choco-arturo <arturo.gonzalez@uniat.edu.mx>
2023-06-02 03:13:16 -0700

    nuevo enemigo

---
+++ Assets/Arturo/Enemigos/Scripts/RangoEnemigo2D.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RangoEnemigo2D : MonoBehaviour
+{
+    public Animator ani;
+    public EnemyBasic enemigo;
+
+    void OnTriggerEnter2D(Collider2D coll)
+    {
+        if(coll.CompareTag("Player"))
+        {
+            ani.SetBool("walk", false);
+            ani.SetBool("run", false);
+            ani.SetBool("attack", true);
+            enemigo.atacando = true;
+            GetComponent<BoxCollider2D>().enabled = false;
+        }
+    }
+
+}
