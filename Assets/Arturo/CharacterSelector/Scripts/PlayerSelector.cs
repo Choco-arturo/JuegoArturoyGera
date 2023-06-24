b681e6ac54ff79ad430c070171c161b238d4b2ff
Choco-arturo <arturo.gonzalez@uniat.edu.mx>
2023-06-23 23:57:44 -0700

    Menu character

--- Assets/Arturo/CharacterSelector/Scripts/PlayerSelector.cs
+++ Assets/Arturo/CharacterSelector/Scripts/PlayerSelector.cs
@@ -1,18 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.Rendering;
+using UnityEngine.UI;
 public class PlayerSelector : MonoBehaviour
 {
+    public Image[] selectionBoxes;
+    public GameObject[] prefabs;
     // Start is called before the first frame update
     void Start()
     {
-        
+        foreach(var img in this.selectionBoxes)
+        {
+            img.gameObject.SetActive(false);
+        }
+        this.Selec(0);
     }
 
-    // Update is called once per frame
-    void Update()
+    public void Selec(int index)
     {
-        
+        foreach (var img in this.selectionBoxes)
+        {
+            img.gameObject.SetActive(false);
+        }
+        this.selectionBoxes[index].gameObject.SetActive(true);
+        PlayerStorage.playerPrefab = this.prefabs[index];
     }
+    
 }
--- Assets/Arturo/CharacterSelector/Scripts/PlayerSpawner.cs
+++ Assets/Arturo/CharacterSelector/Scripts/PlayerSpawner.cs
@@ -7,12 +7,8 @@ public class PlayerSpawner : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Instantiate(PlayerStorage.playerPrefab, this.transform.position, this.transform.rotation);
+        Destroy(this.gameObject);
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
